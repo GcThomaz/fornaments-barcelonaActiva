4962f1db1767e7bdf27eb701be2a84d75413dbe9
GcThomaz <gcthomaz77@gmail.com>
2023-05-09 18:42:22 +0200

    Criaçao branch M5 e adicionando a pasta M5

---
+++ M5/Estudos/ExercicisPraticar/ex02.java
@@ -0,0 +1,28 @@
+package M5.Estudos.ExercicisPraticar;
+
+import java.util.ArrayList;
+
+public class ex02 {
+    public static void main(String[] args) {
+        /*Crie um ArrayList de Strings e adicione alguns nomes de frutas. Em seguida, use um loop for para imprimir cada nome de fruta em uma linha separada.*/
+
+        //Criando a ArrayList
+
+        ArrayList<String> frutas = new ArrayList<>();
+
+        //Adicionando frutas a minha lista
+
+        frutas.add("Morango");
+        frutas.add("Banana");
+        frutas.add("Melancia");
+        frutas.add("Melao");
+
+        //Percorrendo a lista e imprimindo o nome de cada fruta
+
+        for (String fruta: frutas) {
+            System.out.println(fruta);
+        }
+
+
+    }
+}
---
+++ M5/Estudos/ExercicisPraticar/ex03.java
@@ -0,0 +1,30 @@
+package M5.Estudos.ExercicisPraticar;
+
+import java.util.ArrayList;
+
+public class ex03 {
+    public static void main(String[] args) {
+        
+        /*Crie um método que receba um ArrayList de números inteiros e retorne a soma de todos os elementos da lista. */
+
+        //Criando a ArrayList
+
+        ArrayList<Integer>numero = new ArrayList<Integer>();
+
+        //Adicionando itens a ArrayList
+
+        numero.add(1);
+        numero.add(2);
+        numero.add(3);
+        numero.add(4);
+
+        //Retornando a soma dos itens
+
+        int soma = 0;
+
+        for (Integer numeros : numero) {
+            soma += numeros;
+        }
+        System.out.println(soma);
+    }
+}
