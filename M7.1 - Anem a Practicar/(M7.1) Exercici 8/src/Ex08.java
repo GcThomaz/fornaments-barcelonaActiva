4ee5649eb7ea1a51444e3af86bf351122941d889
GcThomaz <gcthomaz77@gmail.com>
2023-06-22 11:44:04 +0200

    create paste

---
+++ M7.1 - Anem a Practicar/(M7.1) Exercici 8/src/Ex08.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Ex08 {
+    public static void main(String[] args) throws Exception {
+        /*Crea un programa on l’usuari/ària introdueixi una temperatura en °C i es mostri per pantalla la conversió en ºFahrenheit
+
+K = 32;
+
+Fórmula
+(0 °C × 9 / 5) + K = 32 °F
+ */
+
+        Scanner in = new Scanner(System.in);
+        
+    }
+}
