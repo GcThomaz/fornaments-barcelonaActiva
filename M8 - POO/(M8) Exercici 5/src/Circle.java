e351aa0cf334af1fc164ce8499a17e932abe2ccf
GcThomaz <gcthomaz77@gmail.com>
2023-06-30 09:39:57 +0200

    Correccion exercici 5 M8

--- M8 - POO/(M8) Exercici 5/src/Circle.java
+++ M8 - POO/(M8) Exercici 5/src/Circle.java
@@ -1,21 +1,21 @@
 public class Circle {
 
     //ATRIBUTTES
-    private int center;
+    private Point center;
     private int radius;
 
     //CONSTRUCTOR    
-    public Circle(int center, int radius) {
+    public Circle(Point center, int radius) {
         this.center = center;
         this.radius = radius;
     }
 
     //GETTERS AND SETTERS
-    public int getCenter() {
+    public Point getCenter() {
         return center;
     }
 
-    public void setCenter(int center) {
+    public void setCenter(Point center) {
         this.center = center;
     }
 
@@ -25,5 +25,62 @@ public class Circle {
 
     public void setRadius(int radius) {
         this.radius = radius;
-    }    
-}
+    }
+	
+	public double distanceCenter(Circle circle) {
+		double distanceX = Math.abs(this.center.getPositionX() - circle.center.getPositionX());
+		double distanceY = Math.abs(this.center.getPositionY() - circle.center.getPositionY());
+		double distance = Math.sqrt(Math.pow(distanceX, 2) + Math.pow(distanceY, 2));
+		return distance;
+	}
+	
+	public String equals(Circle circle) {
+		if (this.center.getPositionX() == circle.center.getPositionX() 
+				&& this.center.getPositionY() == circle.center.getPositionY() && this.radius == circle.getRadius()) {
+			return "Are equals!!!";
+		} else {
+			return "Aren' equals!!!";
+		}		
+	}
+	
+	public String cocentricos(Circle circulo) {
+		if (this.center.getPositionX() == circulo.center.getPositionX() 
+				&& this.center.getPositionY() == circulo.center.getPositionY()) {
+			return "They are cocentric!!!";
+		} else {
+			return "They aren't cocentric!!!";
+		}
+	}
+	
+	public String equalRadius(Circle circle) {
+		if (this.radius == circle.getRadius()) {
+			return "The radio is the same!!!";
+		} else {
+			return "the radio isn't the same";
+		}
+	}
+	
+	public String tangents(Circle circulo) {
+		if (distanceCenter(circulo) == this.radius + circulo.getRadius()) {
+			return "They are tangents!!!";
+		} else {
+			return "They aren't tangents!!!";
+		}
+	}
+	
+	public String secant(Circle circle) {
+		if (distanceCenter(circle) < this.radius + circle.getRadius()) {
+			return "They are secant";
+		} else {
+			return "They aren't secant";
+		}
+	}
+	
+	public String dontTouch(Circle circle) {
+		if (distanceCenter(circle) > this.radius + circle.getRadius()){
+			return "The circles touch each other!!!";
+		} else {
+			return "The circles doesn't touch each other!!!";
+		}
+	}
+	}
--- M8 - POO/(M8) Exercici 5/src/Ex05.java
+++ M8 - POO/(M8) Exercici 5/src/Ex05.java
@@ -3,19 +3,42 @@ import java.util.Scanner;
 public class Ex05 {
     public static void main(String[] args) throws Exception {
 
-    }
-
-    static String askString(String message) {
         Scanner in = new Scanner(System.in);
-        System.out.println(message);
-        //in.close();
-        return in.nextLine();
+        int posicionX;
+        int posicionY;
+        int radius;        
+
+        posicionX = askNumber("The X coordinate is: ");
+
+        posicionY = askNumber("The Y coordinate is: ");
+
+        radius = askNumber("The radius would be: ");
+
+        Point center = new Point(posicionX, posicionY);
+        Circle circle = new Circle(center, radius);
+
+        posicionX = askNumber("The X coordinate is: ");
+
+        posicionY = askNumber("The Y coordinate is: ");
+
+        radius = askNumber("The radius would be: ");
+
+        Point center2 = new Point(posicionX, posicionY);
+        Circle circle2 = new Circle(center2, radius);
+
+        System.out.println("Distance between the two circles:" + circle.distanceCenter(circle2));
+        System.out.println(circle.equals(circle2));
+        System.out.println(circle.cocentricos(circle2));
+        System.out.println(circle.equalRadius(circle2));
+        System.out.println(circle.tangents(circle2));
+        System.out.println(circle.secant(circle2));
+        System.out.println(circle.dontTouch(circle2));
     }
 
     static int askNumber(String message) {
         Scanner in = new Scanner(System.in);
         System.out.println(message);
-        //in.close();
+        // in.close();
         return in.nextInt();
     }
 }
